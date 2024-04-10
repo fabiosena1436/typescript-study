f8ad5e75bef026cc490c3fad7bd50f92a8056baf
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-10 16:06:27 -0300

    Utilizando o tsx, Tipagem Estática

---
+++ src/interfaces-and-custom-types.ts
@@ -0,0 +1,54 @@
+type Coordinte = {
+    x: number
+    y: number
+}
+
+const coordinte: Coordinte = {
+    x: 20,
+    y: 8
+}
+
+function calculateDistnace(pointA: Coordinte, pointB: Coordinte): number {
+    const dx = pointB.y - pointA.x;
+    const dy = pointB.y - pointA.x;
+    const result = Math.sqrt(dx * dx + dy * dy)
+
+    return result
+}
+
+const coordinteA: Coordinte = {
+    x: 20,
+    y: 8
+}
+
+const coordinteB: Coordinte = {
+    x: 15,
+    y: 30
+}
+
+console.log(calculateDistnace(coordinteA, coordinteB))
+
+///////////////////////////////////////////
+
+interface Car {
+    brand: string;
+    model: string;
+    year: number;
+    startEngine: () => void;
+}
+
+const myCar: Car = {
+    brand: 'Audi',
+    model:'A3',
+    year: 2024,
+    startEngine:()=> {
+        console.log('motor iniciado')
+    }
+}
+
+function drive(car:Car):void{
+    console.log(`Driving${car.brand}- ${car.model}`)
+    car.startEngine()
+}
+
+drive(myCar)
---
+++ src/static-typing.ts
@@ -0,0 +1,35 @@
+let age: number = 27
+let fullName: string = "Kurt"
+let isActive: boolean = false
+
+// Inferencia de Tipos
+
+let animal = 'Cat';
+let quantity = 23;
+
+//Parametros e retonos de funçoes
+
+function sum(a: number, b: number): number {
+    const result = a + b
+    return result
+}
+
+// Funçao com retorno opcional
+function greeting(fullName: string): string | void {
+
+    if (fullName) {
+        return `hello,${fullName}!`
+    }
+
+}
+
+// Funçao com parametros opcionais
+function stringOrNumberSize(value: string | number): number {
+    if (typeof value === 'string') {
+        return value.length
+    }
+    return value
+}
+
+console.log(stringOrNumberSize( 'hello'))
+console.log(6)
