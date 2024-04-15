ef38513a146b34c6919df70fed41af2ed01562c1
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-15 15:59:53 -0300

    att

--- src/static-typing.ts
+++ src/static-typing.ts
@@ -1,35 +1,33 @@
-let age: number = 27
-let fullName: string = "Kurt"
-let isActive: boolean = false
+let age: number = 27;
+let fullName: string = "Kurt";
+let isActive: boolean = false;
 
 // Inferencia de Tipos
 
-let animal = 'Cat';
+let animal = "Cat";
 let quantity = 23;
 
 //Parametros e retonos de funçoes
 
 function sum(a: number, b: number): number {
-    const result = a + b
-    return result
+  const result = a + b;
+  return result;
 }
 
 // Funçao com retorno opcional
 function greeting(fullName: string): string | void {
-
-    if (fullName) {
-        return `hello,${fullName}!`
-    }
-
+  if (fullName) {
+    return `hello,${fullName}!`;
+  }
 }
 
 // Funçao com parametros opcionais
 function stringOrNumberSize(value: string | number): number {
-    if (typeof value === 'string') {
-        return value.length
-    }
-    return value
+  if (typeof value === "string") {
+    return value.length;
+  }
+  return value;
 }
 
-console.log(stringOrNumberSize( 'hello'))
-console.log(6)
+console.log(stringOrNumberSize("hello"));
+console.log(6);
--- src/utility-types.ts
+++ src/utility-types.ts
@@ -1,25 +1,25 @@
 type User = {
-    id: number;
-    username: string;
-    password: string;
-    createAt: Date;
-    updtedAt: Date;
-}
+  id: number;
+  username: string;
+  password: string;
+  createAt: Date;
+  updtedAt: Date;
+};
 
 const user: User = {
-    id: 1,
-    username: 'agustinho',
-    password: 'tipooe123456',
-    createAt: new Date(),
-    updtedAt: new Date(),
-}
+  id: 1,
+  username: "agustinho",
+  password: "tipooe123456",
+  createAt: new Date(),
+  updtedAt: new Date(),
+};
 
 type PartiaUser = Partial<User>;
 
-type NewUser = Omit<User, 'id' | 'username'>;
+type NewUser = Omit<User, "id" | "username">;
 
-type ShearshUser = Pick<User, 'username'>;
+type ShearshUser = Pick<User, "username">;
 
 const shearshUser: ShearshUser = {
-    username: 'agosrinho'
-}
+  username: "agosrinho",
+};
